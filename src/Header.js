da302c3861fce193719958c3bcc7795d0885b731
aryand7265@gmail.com <aryand7265@gmail.com>
2025-06-23 16:31:40 +0530

    Resolved merge conflict in all

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
@@ -93,27 +92,4 @@ function Header() {
         </header>
   );
 }
-
 export default Header;
-=======
-import React from 'react'
-import './Header.css'
-
-function Header() {
-  return (
-    <div>
-        <header className='container'>
-            <h2>Aryan <span style={{color:'rgb(113, 113, 205)'}}>Dewangan</span></h2>
-            <ul className='sub_container'>
-                <li style={{color:'rgb(113, 113, 205)'}}>Home</li>
-                <li>About</li>
-                <li>Project</li>
-                <li>Contact</li>
-            </ul>
-        </header>
-    </div>
-  )
-}
-
-export default Header
->>>>>>> 64ed7a1abf1b5cbe73fe4b7c8a747ddeb91f09d4
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -77,33 +76,4 @@ function Home() {
 }
 
 export default Home;
-=======
-import React from 'react'
-import Image1 from './Image/myImg.JPG'
-import './Home.css'
 
-function Home() {
-    return (
-        <>
-            <div className="container1">
-                <div className="sub_container1">
-                    <h2 className='starter'>Hello,&nbsp;<span>I'm</span></h2>
-                    <p className="name">Aryan Dewangan</p>
-                    <p className="job">Full Stack Developer</p>
-                    <p className='description'>I am a full stack developer. I enjoy writing clean code, building user interfaces expert and more.</p>
-                    <button className='resume'>Resume</button>
-                </div>
-                <div className="sub_container2">
-                            <img className='myimg' src={Image1}/>
-                </div>
-
-
-            </div>
-
-
-        </>
-    )
-}
-
-export default Home
->>>>>>> 64ed7a1abf1b5cbe73fe4b7c8a747ddeb91f09d4
